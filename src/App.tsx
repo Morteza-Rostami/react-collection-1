a0cd66da3fe83f3cf0b09e2886a66acef63c754c
Morteza Rostami <morteza.rostami.55b@gmail.com>
2023-07-07 07:30:11 -0700

    3: third time testing github pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import useRunOnce from './hooks/useRunOnce';
+//import useRunOnce from './hooks/useRunOnce';
 import CommentsPage from './pages/CommentsPage';
 
 function App() {
